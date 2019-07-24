011880d51166db723cff822e38677f73d5bf7028
Alex Macniven <macniven.ap@gmail.com>
2019-07-24 21:27:02 +0100

    Adds Index Template
    In this commit an index template is added and index route is updated to
    use it.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World"
-
+    # Jinja2 will apply 'user' to the placeholder in index.html
+    user = dict(username = "Alex")
+    return render_template("index.html", title="Home", user=user)
