6138a5b339d2c505707d16b124e3b9d69dbe23bd
Alex Macniven <macniven.ap@gmail.com>
2019-08-06 20:57:47 +0100

    SQLAlchemy and Migrate Base Implementation
    In this commit, I added the basic
    implementations for SQLAlchemy and
    Migrate.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
+
 
 app = Flask(__name__)
 
 # Load the Config class as the applications configuration
 app.config.from_object(Config)
+
+# Initialise the SQLAlchemy database.
+db = SQLAlchemy(app)
+
+# Initialise the Migrate object.
+migrate = Migrate(app, db)
 
 # Routes is going to import 'app' so putting the import here will avoid
 # circular imports.
